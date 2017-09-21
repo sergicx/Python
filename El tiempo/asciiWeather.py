1060caad8e2f60f1e345cffe9abc336409f7a44d
Sergi Cañuelo <sergicx96@gmail.com>
2017-09-21 11:29:20 +0200

    Add files via upload

---
+++ El tiempo/asciiWeather.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+asciiSun = "  \   /    \n   .-.\n- (   ) -  \n   '-'      \n  /   \ "
+asciiPartiallyCloud = "    \  /\n  _ /''.-.\n    \_(   ).   \n    /(___(__) "
+asciiCloud = "   .--.     \n .-(    ).   \n(___.__)__)"
+asciiRain = "  .-. \n  (   ).\n (___(__)\n  ' ' ' '\n ' ' ' '"
